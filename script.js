b750f576c86ab67c9aa1b71653f7dd472406e3a0
HarxhXT <harshbankey09@gmail.com>
2026-01-18 10:13:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Welcome to HarxhXT Portfolio 🚀");
+const text = [
+  "Minecraft Server Manager",
+  "Web Developer",
+  "Event Organizer",
+  "Plugin Configurator"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+  if (count === text.length) count = 0;
+  currentText = text[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typing").textContent = letter;
+
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(type, 1500);
+  } else {
+    setTimeout(type, 120);
+  }
+})();
