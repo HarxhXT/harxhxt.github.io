22526e15788bd081650c7c215388643278832db8
Harsh <harshbankey09@gmail.com>
2026-01-19 20:10:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const cards = document.querySelectorAll(".card");
-
-window.addEventListener("scroll", () => {
-  cards.forEach(card => {
-    const top = card.getBoundingClientRect().top;
-    if (top < window.innerHeight - 100) {
-      card.style.opacity = 1;
-      card.style.transform = "translateY(0)";
-    }
-  });
-});
+console.log("Portfolio Loaded Successfully");
